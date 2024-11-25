c6490dc8d06e54f5c81794c5f51a2cc22d10908f
Ваше имя пользователя <rudenkodanila21fx@gmail.com>
2024-11-26 04:26:25 +0700

    lmao

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+// Конфигурация для сервера
+const serverConfig = {
     target: 'node',
     mode: 'production',
     entry: './server.js',
@@ -25,3 +26,35 @@ module.exports = {
         ]
     }
 };
+
+// Конфигурация для клиентских файлов
+const clientConfig = {
+    target: 'web',
+    mode: 'production',
+    entry: {
+        main: './main.js',
+        game: './game.js',
+        friends: './friends.js',
+        tasks: './tasks.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = [serverConfig, clientConfig];
