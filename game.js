7c6b92f640d5571f3475574fd5e7f0b98529b66f
Ваше имя пользователя <rudenkodanila21fx@gmail.com>
2024-11-27 21:15:23 +0700

    ffix

--- game.js
+++ game.js
@@ -263,6 +263,8 @@ startButton.addEventListener('click', async () => {
         }
         // Начисляем очки
         const gameScore = parseInt(startButton.dataset.pendingScore);
+        await updateBalance(gameScore, 'game');
+
         totalDPS += gameScore;
         totalGameEarnings += gameScore;
         localStorage.setItem('totalDPS', totalDPS);
